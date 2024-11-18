6235bbcc5feafa724e76d9d149140774f4b9c6a9
mashakrot <makrosensei@gmail.com>
2024-11-18 20:33:03 +0200

    12

--- src/index.ts
+++ src/index.ts
@@ -51,8 +51,18 @@ router.get("/users", (req: Request, res: Response) => {
     // let users: any = req.params
     console.log(users);
     
-    res.json({ users });
+    // // try{
+    // //     res.json(poems[id])
+    // // } catch (error: any) {
+    // //     console.error(`Error parsing JSON: ${error}`)
+    // // }
+    // res.json({ users });
+    
+    res.status(201).json({ users });
 })
+
+
+
 
 
 export default router
